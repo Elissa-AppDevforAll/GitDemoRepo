8d8be74d7ee5cd14dca628054b1bab1ffc86bcde
Elissa-AppDevforAll <elissamiller@appdevforall.org>
2026-05-21 16:46:24 -0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = "Git in Code on the Go"
+
+include(":app")
